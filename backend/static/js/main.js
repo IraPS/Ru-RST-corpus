80df43d887cf09dcd06d991119e82fd4812d7b2d
sonniki <son.nik@mail.ru>
2017-11-10 11:24:59 +0300

    Update main.js

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -1,11 +1,1 @@
-$(function () {
-	$('form[name="contact"]').submit(function () {
-		var $this = $(this);
-		var message = $this.find('input[name="messagetext"]').val();
-		if (mesage === "") {
-			alert('Поле "Сообщение" не должно быть пустым.');
-		} else {
-			alert('Ваше сообщение отправлено!');
-		}
-	});
-});
+
