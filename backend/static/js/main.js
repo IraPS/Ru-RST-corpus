5fb84e8d6a762d878a3d2630b733bff28b0b6d78
sonniki <son.nik@mail.ru>
2017-11-12 23:13:01 +0300

    Update main.js

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -1,1 +1,21 @@
+$('form').submit(function () {
+	var $form = $(this),
+    val_list = [];
+  $form.find('.block').each(function () {
+    var $block = $(this),
+      input_type = $block.find('select').val(),
+      input_text = $block.find('.form-control mb-2 mr-sm-2 mb-sm-0').val();
+    val_list.push({
+    	type: input_type,
+    	text: input_text
+    });
+  });
+  alert(JSON.stringify({data: val_list}));
+  return false;
+});
 
+$('.add').click(function () {
+	var $block = $(this).parent();
+  $block.clone(true).insertBefore('.btn btn-primary');
+  return false;
+});
