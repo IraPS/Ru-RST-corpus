054a835d29ba5c2b955911f8b9c9c5140625b1bf
sonniki <son.nik@mail.ru>
2017-11-18 18:47:36 +0300

    Update main.js

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -17,12 +17,34 @@ $('form').submit(function () {
 
 $('.add').click(function () {
 	var $block = $(this).parent();
-  $block.clone(true).insertBefore('.submit');
-  return false;
-}); 
+	var $newblock = $block.clone(true);
+	if ($newblock.find('.placeForSearch').innerHTML != '') {
+		$newblock.find('.placeForSearch').empty();
+	} 
+	$newblock.insertBefore('.submit');
+	return false;
+});
+	
 $('.closeimg').click(function () {
 	var $block = $(this).parent();
   $block.remove();
   return false;
-}); 
+});
+	
+$('.selectSearchOpt').change(function () {
+  var $block = $(this).parent();
+  var choice = $block.find('.selectSearchOpt').val();
+  if (choice === 'word') {
+    $block.find('.placeForSearch').get(0).innerHTML = '<input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Поиск слова...">';
+  } else if (choice === 'lemma') {
+    $block.find('.placeForSearch').get(0).innerHTML = '<input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Поиск леммы...">';
+  } else if (choice === 'pos') {
+    $block.find('.placeForSearch').get(0).innerHTML = '<select><option value="" selected>Выберите часть речи...</option><option value="noun">Существительное</option><option value="verb">Глагол</option><option value="adj">Прилагательное</option><option value="adv">Наречие</option><option value="pronoun">Местоимение</option><option value="preposition">Предлог</option><option value="conj">Союз</option><option value="particle">Частица</option></select>';
+  } else if (choice === 'marker') {
+    $block.find('.placeForSearch').get(0).innerHTML = '<select><option value="" selected>Выберите маркер РО...</option><option value="potomuchto">потому что</option><option value="poetomu">поэтому</option></select>';
+  } else {
+    $block.find('.placeForSearch').get(0).innerHTML = null;
+  }
+});
+	
 });
