47ab499fcd3adc890bfc67bd56096479aa858881
sonniki <son.nik@mail.ru>
2017-12-06 16:15:58 +0300

    Update all functions

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -2,10 +2,10 @@ $(function() {
     $('#searchform').submit(function() {
         var $form = $(this),
             val_list = [];
-        $form.find('.block').each(function() {
+        $form.find('.block:visible').each(function() {
             var $block = $(this);
-            var input_type = $block.find('select').val();
-            var input_ro = $block.find('.selectpicker').val();
+            var input_type = $block.find('.selectSearchOpt').val();
+            var input_ro = $block.find("select.multselect_ro").val();
             if (input_type == "word" || input_type == "lemma") {
                 var input_text = $block.find('input').val();
             } else {
@@ -25,43 +25,39 @@ $(function() {
         return false;
     });
 
-    $('.add').click(function() {
-        var $block = $(this).parent();
-        var $newblock = $block.clone(true);
-        if ($newblock.find('.placeForSearch').innerHTML != '') {
-            $newblock.find('.placeForSearch').empty();
+    var clone_block = function() {
+		var $newblock = $("#searchform .block:hidden").clone();
+		var $inserted = $newblock.insertBefore('.submit').show();
+		$inserted.find(".multselect_ro").selectpicker();
+		$inserted.find('.selectSearchOpt').change(function() {
+			var $searchopt = $(this);
+			var $parblock = $searchopt.parent();
+			var choice = $searchopt.val();
+			if (choice === 'word') {
+				$parblock.find(".searchOption").html('<input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="Поиск слова...">');
+			} else if (choice === 'lemma') {
+				$parblock.find(".searchOption").html('<input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="Поиск леммы...">');
+			} else if (choice === 'pos') {
+				$parblock.find(".searchOption").html('<select class="dropdown select"><option value="" selected>Выберите часть речи...</option><option value="S">Существительное</option><option value="V">Глагол</option><option value="A">Прилагательное</option><option value="ADV">Наречие</option><option value="SPRO">Местоимение</option><option value="PR">Предлог</option><option value="CONJ">Союз</option><option value="PART">Частица</option></select>');
+			} else if (choice === 'marker') {
+				$parblock.find(".searchOption").html('<select class="dropdown select"><option value="" selected>Выберите маркер РО...</option><option value="potomuchto">потому что</option><option value="poetomu">поэтому</option></select>');
+			} else {
+				$parblock.find(".searchOption").html(null);
+			}
+		});
+		$inserted.find(".add").click(function() {
+			var $blockselect = clone_block();
+			console.log($blockselect.find(".selectpicker"));
+			return false;
+		});
+		$inserted.find(".closeimg").click(function() {
+			var $block = $(this).closest(".block");
+			$block.remove();
+			return false;
+		});
+		return $inserted;
+	};
 
-        }
-        var selected_ro = $block.find('.selectpicker').val();
-        if (selected_ro != "any") {
-            $newblock.find('.selectpicker').selectpicker('val','any');
-        }
-        $newblock.insertBefore('.submit');
-        $newblock.find('.selectpicker').selectpicker('deselectAll');
-        $newblock.find("selectpicker").attr("id", "new");
-        return false;
-    });
-
-    $('.closeimg').click(function() {
-        var $block = $(this).parent();
-        $block.remove();
-        return false;
-    });
-
-    $('.selectSearchOpt').change(function() {
-        var $block = $(this).parent();
-        var choice = $block.find('.selectSearchOpt').val();
-        if (choice === 'word') {
-            $block.find('.placeForSearch').get(0).innerHTML = '<input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="Поиск слова...">';
-        } else if (choice === 'lemma') {
-            $block.find('.placeForSearch').get(0).innerHTML = '<input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="Поиск леммы...">';
-        } else if (choice === 'pos') {
-            $block.find('.placeForSearch').get(0).innerHTML = '<select class="dropdown select"><option value="" selected>Выберите часть речи...</option><option value="S">Существительное</option><option value="V">Глагол</option><option value="A">Прилагательное</option><option value="ADV">Наречие</option><option value="SPRO">Местоимение</option><option value="PR">Предлог</option><option value="CONJ">Союз</option><option value="PART">Частица</option></select>';
-        } else if (choice === 'marker') {
-            $block.find('.placeForSearch').get(0).innerHTML = '<select class="dropdown select"><option value="" selected>Выберите маркер РО...</option><option value="potomuchto">потому что</option><option value="poetomu">поэтому</option></select>';
-        } else {
-            $block.find('.placeForSearch').get(0).innerHTML = null;
-        }
-    });
+	clone_block();
 
 });
